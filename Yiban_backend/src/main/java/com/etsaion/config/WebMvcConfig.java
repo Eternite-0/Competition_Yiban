9f1be7ed7654078a644aa691bc66c08f785791d0
Éternité <arithmo9@gmail.com>
2026-06-05 16:34:18 +0800

    fix: CORS 允许所有来源，支持 Cloudflare 隧道访问

--- Yiban_backend/src/main/java/com/etsaion/config/WebMvcConfig.java
+++ Yiban_backend/src/main/java/com/etsaion/config/WebMvcConfig.java
@@ -54,7 +54,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Correlation-Id", "Accept")
                 .exposedHeaders("X-Correlation-Id")
